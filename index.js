c47edcd47781155e29937d7271407e8b856741cd
MrHarsh002 <harshkumar26100@gmail.com>
2025-08-25 16:15:26 +0530

    routechange

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import dbConnect from './config/dbConfig.js'
+import userRouter from "./routes/userRoutes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+app.use('/user', userRouter);
 
 app.use('/',(res, req) => {
     res.send("All Good")
